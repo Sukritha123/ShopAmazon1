043e019c8b90f47c08c263d80bd0f39247b30411
Sukritha123 <144372708+Sukritha123@users.noreply.github.com>
2023-09-21 16:57:04 +0100

    added demoblaze tests

--- cypress/integration/CrossDomain/crossDomain.js
+++ cypress/integration/CrossDomain/crossDomain.js
@@ -9,29 +9,32 @@ describe('Test Suit for Cross Domain', function () {
 
         commitquality.getTFilterTextBox().type("Test")
         commitquality.getTFilterButton().click()
-        commitquality.getTTextMsg().should('include.text','No products found')
+        commitquality.getTTextMsg().should('include.text', 'No products found')
         commitquality.getTResetButton().click()
         commitquality.getTTextMsg().should('not.exist')
 
-        cy.visit("https://www.npmjs.com");  //visit a secondary domain and perform test
-        cy.origin("https://www.npmjs.com",()=> {
-            const Npmpage = Cypress.require('../../support/pageObjects/Npmpage')
-            const NpmPropage = Cypress.require('../../support/pageObjects/NpmPropage')
-            const npmpage = new Npmpage()
-            const npmpropage = new NpmPropage()
+        cy.visit("https://www.demoblaze.com/");  //visit a secondary domain and perform test
+        cy.origin("https://www.demoblaze.com/", () => {
+            const Demoblazepage = Cypress.require('../../support/pageObjects/Demoblazepage')
+            const demoblazepage = new Demoblazepage()
 
-            npmpage.getPageURL().should('eq', 'https://www.npmjs.com/')
-            npmpage.getHeadline().should('include.text','Build amazing things')
-            npmpage.getTabPro().click()
-            npmpropage.getProPageURL().should('include', '/products/pro')
+            demoblazepage.getPageURL().should('eq', 'https://www.demoblaze.com/')
+            demoblazepage.getTabContact().click()
+            demoblazepage.getContactModalTitle().should('have.text', 'New message')
+            demoblazepage.getContactModalTextbox().type('Test')
+            demoblazepage.getContactModalSendBtn().click()
+            cy.on('window:alert', (str) =>  //Capture window alert message and validate 
+            {
+                expect(str).to.equal('Thanks for the message!!')
+            })
         })
 
         cy.visit("https://commitquality.com/"); //return to primary domain and continue test
-        commitquality.getTableRows().should('have.length',10)
+        commitquality.getTableRows().should('have.length', 10)
         commitquality.getShowMoreButton().click()
-        commitquality.getTableRows().should('have.length',11)
+        commitquality.getTableRows().should('have.length', 11)
 
-        
+
 
 
     })
